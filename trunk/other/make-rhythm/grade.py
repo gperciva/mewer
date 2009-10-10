33d8611505d4a32d77df6a28dc11584c63934f26
percival.music.ca <percival.music.ca@bd786f4e-b8f1-11dd-b55b-2da8d274f894>
2009-10-10 13:49:57 +0000

    Refactor code.

--- trunk/other/make-rhythm/grade.py
+++ trunk/other/make-rhythm/grade.py
@@ -8,24 +8,70 @@ import glob
 WIDTH = 400
 HEIGHT = 50
 
+try:
+        perfect_name = sys.argv[1]
+except:
+        print "Please enter perfect exercise filename"
+        sys.exit(1)
+
+
+def getOnsets(filename):
+	list = []
+	lines = open(filename, 'r').readlines()
+	for line in lines:
+		note = float(line.split()[0])
+		if (note > 0):
+			time = float(line.split()[1])
+			list.append(time)
+	return list
+
+
+def findNearest(list, value):
+	bestDelta = abs(float(list[0]) - value)
+	for i in len(list):
+		delta = abs(list[i] - value)
+		if (delta < bestDelta):
+			bestDelta = delta
+	return bestDelta
+
+def gradeExercise(perfect, exercise, offset):
+	error = 0.0
+	for line in perfect:
+		note = float(line.split()[0])
+		if (note==0):
+			continue
+		time = float(line.split()[1])
+		noteError = findNearest(exercise, time-offset)
+
+	for line in exercise:
+		print foo
+
+
+perfect = getOnsets(perfect_name)
+
 scores = []
-for exp in glob.glob('*.exp'):
+for exp in glob.glob('*-*.exp'):
 	base = exp.split('-')[0]
 	number = exp.split('-')[1].split('.')[0]
-	print base
-	print number
+
+	onsets = getOnsets(exp)
+
+	averageOnsets = float(sum(onsets)) / len(onsets)
+	averagePerfect = float(sum(perfect)) / len(perfect)
+	offset = averagePerfect - averageOnsets
+
+	# draw png
 	image = Image.new('RGB', [WIDTH, HEIGHT], 'white')
 	draw = ImageDraw.Draw(image)
-
-	onsets = open(exp, 'r').readlines()
-	for line in onsets:
-		note = float(line.split()[0])
-		time = float(line.split()[1])
-		xpos = time * WIDTH / 8.0
-		print xpos
-		if (note>0):
-			draw.line((xpos,0, xpos,HEIGHT),fill='blue')
-	
+	for o in onsets:
+		xpos = (o + offset) * WIDTH / 8.0
+		draw.line((xpos,0, xpos,HEIGHT),fill='blue')
 	image_name = base+'-'+number+'.png'
 	image.save(image_name, 'png')
+
+
+	sys.exit(1)
+	gradeExercise(perfect, onsets, offset)
+
+
 
