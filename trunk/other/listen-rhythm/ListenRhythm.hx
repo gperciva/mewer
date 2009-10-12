2f462d19a523e86c3812cd48c08071452338a347
percival.music.ca <percival.music.ca@bd786f4e-b8f1-11dd-b55b-2da8d274f894>
2009-10-12 12:41:03 +0000

    Architecture

--- trunk/other/listen-rhythm/ListenRhythm.hx
+++ trunk/other/listen-rhythm/ListenRhythm.hx
@@ -1,11 +1,27 @@
 class ListenRhythm {
 
+	public function new() {
+		showMain();
+	}
+
+	function showMain() {
+		var tf = new flash.text.TextField();
+		tf.text = "Rhythmic Grading";
+		tf.x = 110;
+		tf.y = 20;
+		flash.Lib.current.addChild(tf);
+
+	}
+
 	static function main() {
+		new ListenRhythm();
+/*
+		showMain();
 		trace("foo");
 
         var s:flash.display.MovieClip = flash.Lib.attach("foo-1-png");
         flash.Lib.current.addChild(s);
-
+*/
 	}
 
 }
