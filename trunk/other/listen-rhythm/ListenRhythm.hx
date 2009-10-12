69df587684bbba1fee0636814d750a4b30f0b054
percival.music.ca <percival.music.ca@bd786f4e-b8f1-11dd-b55b-2da8d274f894>
2009-10-12 10:37:58 +0000

    Displaying an image.

--- trunk/other/listen-rhythm/ListenRhythm.hx
+++ trunk/other/listen-rhythm/ListenRhythm.hx
@@ -1,7 +1,25 @@
+
 class ListenRhythm {
 
 	static function main() {
-		trace ("hello world");
+		trace("foo");
+
+		var s:flash.display.MovieClip = flash.Lib.attach("foo-1-png");
+		//var foo = flash.Lib.current.attach("foo-1-png");
+		//foo._x = 40;
+		//foo._x = 40;
+		flash.Lib.current.addChild(s);
+
+/*
+        var mc:flash.display.MovieClip = flash.Lib.current;
+        mc.graphics.beginFill(0xFF0000);
+        mc.graphics.moveTo(50,50);
+        mc.graphics.lineTo(100,50);
+        mc.graphics.lineTo(100,100);
+        mc.graphics.lineTo(50,100);
+        mc.graphics.endFill();
+*/
+
 	}
 
 }
