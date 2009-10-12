c62f40e55bb32008eb592cc7c0272393122c64fd
Graham Percival <graham@percival-music.ca>
2009-10-12 11:53:25 +0000

    Use flash 9.

--- other/listen-rhythm/ListenRhythm.hx
+++ other/listen-rhythm/ListenRhythm.hx
@@ -1,24 +1,10 @@
-
 class ListenRhythm {
 
 	static function main() {
 		trace("foo");
 
-		var s:flash.display.MovieClip = flash.Lib.attach("foo-1-png");
-		//var foo = flash.Lib.current.attach("foo-1-png");
-		//foo._x = 40;
-		//foo._x = 40;
-		flash.Lib.current.addChild(s);
-
-/*
-        var mc:flash.display.MovieClip = flash.Lib.current;
-        mc.graphics.beginFill(0xFF0000);
-        mc.graphics.moveTo(50,50);
-        mc.graphics.lineTo(100,50);
-        mc.graphics.lineTo(100,100);
-        mc.graphics.lineTo(50,100);
-        mc.graphics.endFill();
-*/
+        var s:flash.display.MovieClip = flash.Lib.attach("foo-1-png");
+        flash.Lib.current.addChild(s);
 
 	}
 
