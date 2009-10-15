eb5aed5777e41588429bd2a26c214663973a71c4
Graham Percival <graham@percival-music.ca>
2009-10-15 12:16:20 +0000

    Playing a sound.

--- other/listen-rhythm/Attempt.hx
+++ other/listen-rhythm/Attempt.hx
@@ -1,10 +1,14 @@
+class Foo extends flash.media.Sound {}
+
 class Attempt {
 	var phaseNumber : Int;
 	var realNumber : Int;
+	var name : String;
 
 	public function new(phaseNumberGet : Int, realNumberGet : Int) {
 		phaseNumber = phaseNumberGet;
 		realNumber = realNumberGet;
+		name = Std.string(phaseNumber)+"-"+Std.string(realNumber);
 	}
 
 	public function selected() {
@@ -12,16 +16,24 @@ class Attempt {
 	}
 
 	public function imageName() {
-		return Std.string(phaseNumber) + "-" +
-			Std.string(realNumber) + "-png";
+		return name + "-png";
 	}
 
 	public function startPlay() {
-
+		var soundFile = name + "-mp3";
+		trace("trying to load sound");
+		var sound : flash.media.Sound = new Foo();
+/*
+		var sound : flash.media.Sound = Type.createInstance(
+			Type.resolveClass("resources.classes.1-1-mp3"), []);
+*/
+		trace(sound.length);
+		sound.play();
+		trace("trying to play");
 	}
 
 	public function stopPlay() {
-
+		trace("stop playing " + name);
 	}
 
 }
