e3569c6a5eb073cbaee99091c8a7e87decea0308
Graham Percival <graham@percival-music.ca>
2009-11-24 13:19:08 +0000

    Adding a metronome.

--- other/second-rhythm/exp2wav.py
+++ other/second-rhythm/exp2wav.py
@@ -5,52 +5,91 @@ import struct
 
 inclap_name = "clap.wav"
 try:
-	inpositions_name = sys.argv[1]
+	tempo = float(sys.argv[1])
+except:
+	print "Please enter the tempo (in BPM)"
+	sys.exit(1)
+try:
+	inpositions_name = sys.argv[2]
 except:
 	print "Please enter input position filename"
-	sys.exit(1)
+	sys.exit(2)
 
+beat_samples = 44100.0 / (60.0/tempo)
 outfile_name = inpositions_name[:-4] + ".wav"
 
 
-print "Reading clap position file " + inpositions_name
+#print "Reading clap position file " + inpositions_name
 positions = open(inpositions_name, 'r').readlines()
 
-print "Reading input clap file " + inclap_name
+#print "Reading input clap file " + inclap_name
 inclap_file = wave.open(inclap_name, 'rb')
 clap = inclap_file.readframes(4410)
 inclap_file.close()
+
+metro_dur = 441
+inmetro_high_file = wave.open('metro-high.wav', 'rb')
+metro_high = inmetro_high_file.readframes(4410)
+inmetro_high_file.close()
+
+inmetro_low_file = wave.open('metro-low.wav', 'rb')
+metro_low = inmetro_low_file.readframes(4410)
+inmetro_low_file.close()
+
 
 print "Writing output file " + outfile_name
 out_file = wave.open(outfile_name, 'wb')
 out_file.sr = 44100
 out_file.setparams((1, 2, 44100, 44100*4, 'NONE', 'noncompressed'))
 
-# initial silence; currently needed in MEAWS.  500 ms
-fill_zeros = ''
-for i in range( 22050 ):
-	fill_zeros += struct.pack('h', 0)
-out_file.writeframes(fill_zeros)
 
+def silenceSamples(samples):
+	fill_zeros = ''
+	for i in range(int(samples)):
+		fill_zeros += struct.pack('h', 0)
+	return fill_zeros
+
+
+# two bars of 4/4 time
+metronome = ''
+for bar in range(2):
+	metronome += metro_high
+	metronome += silenceSamples(beat_samples - metro_dur)
+	for offbeat in range(3):
+		metronome += metro_low
+		metronome += silenceSamples(beat_samples - metro_dur)
+
+claps = ''
 now = 0
 for line in positions:
 	splitline = line.split()
 	note = float(line.split()[0])
 	time = float(line.split()[1])
 
-	# fill zeros
-	fill_zeros = ''
-	num_zeros = int( 44100.0*(time-now))
-	if (num_zeros > 0):
-		for i in range( num_zeros ):
-			fill_zeros += struct.pack('h', 0)
-		out_file.writeframes(fill_zeros)
-		now = time
 	if (note > 0):
-		out_file.writeframes(clap)
+		num_zeros = int( 44100.0*(time-now))
+		if (num_zeros > 0):
+			claps += silenceSamples(num_zeros)
+		now = time
+		claps += clap
 		now += 0.1   # length of clap
+# fill remainder with zeros
+remaining_zeros = (len(metronome) - len(claps))/2
+if (remaining_zeros < 0):
+	print "ERROR: claps exceed a bar!"
+	sys.exit()
+if (remaining_zeros > 0):
+	claps += silenceSamples(remaining_zeros)
 
 
-out_file.close()
+# mix them together
+audioframes = ''
+size = len(metronome)/2
+metro_values = struct.unpack(str(size)+'h', metronome)
+clap_values = struct.unpack(str(size)+'h', claps)
+for i in range(size):
+	audioframes += struct.pack('h',metro_values[i]+clap_values[i])
+	
+out_file.writeframes(audioframes)
 
 
